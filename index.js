5a020017c8889d438d93dd74926ac5087fa24e81
Ken Faubel <ken@faubel.org>
2021-08-10 06:33:38 -0400

    Add index.d.ts

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 // Use app.ts for local testing and to build a stand alone solution
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const RedCedarsImage = require("./build/RedCedarsImage");
-module.exports = RedCedarsImage;
+const RedCedarsBuilder = require("./build/RedCedarsBuilder");
+module.exports = RedCedarsBuilder;
